2c9e929ae11df3fa591399c56230032a1a140d1e
rrmm76 <94173113+rrmm76@users.noreply.github.com>
2023-02-05 16:50:38 +0200

    change formate

--- src/components/ExpenseListItems.js
+++ src/components/ExpenseListItems.js
@@ -1,12 +1,17 @@
 import React  from "react";
 import { connect } from "react-redux";
 import  {Link } from 'react-router-dom';
-import {removeExpense} from "../actions/expenses"
+import moment from 'moment';
+import numeral from "numeral";
 
 const ExpenseListItems =({dispatch , id ,description,amount,createdAt}) => (
     <div>
     <Link to={`/edit/${id}`}> <h3>{description}</h3></Link>
-    <p>{amount}  - {createdAt}</p>
+    <p>
+    {numeral(amount /100).format('$0.0,00')}
+    - 
+    {moment(createdAt).format('MMM DD,YYYY')}
+    </p>
         
     </div>
     
